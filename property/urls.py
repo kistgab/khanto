3263c465b1b39db9454b00b785f589dc38e018b4
kistgab <gabrielkist02@gmail.com>
2024-03-26 12:58:19 -0300

    feat: add property crud

---
+++ property/urls.py
@@ -0,0 +1,6 @@
+from property.views import PropertyViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'', PropertyViewSet)
+urlpatterns = router.urls
--- property/views.py
+++ property/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from property.models import Property
+from property.serializers import PropertySerializer
 
 # Create your views here.
+class PropertyViewSet(viewsets.ModelViewSet):
+  queryset = Property.objects.all()
+  permission_classes = [permissions.AllowAny]
+  serializer_class = PropertySerializer
