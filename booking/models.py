df96fcf1b9ca5cee0eb1004dc5299f8075afe1c0
kistgab <gabrielkist02@gmail.com>
2024-03-26 08:34:17 -0300

    feat: add booking model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from advertisement.models import Advertisement
+
 # Create your models here.
+class Booking(models.Model):
+  advertisement = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
+  check_in_date = models.DateField()
+  check_out_date = models.DateField()
+  total_value = models.DecimalField(max_digits=10, decimal_places=2)
+  comments = models.TextField()
+  total_guests = models.SmallIntegerField()
+  creation_date = models.DateTimeField(auto_now_add=True)
+  last_updated = models.DateTimeField(auto_now=True)
