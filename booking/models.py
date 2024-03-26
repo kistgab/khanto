3e8276fb912b881080d0e9724788c2340d2d67d5
kistgab <gabrielkist02@gmail.com>
2024-03-26 10:41:43 -0300

    refactor: make Bookin Id random

--- booking/models.py
+++ booking/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 from advertisement.models import Advertisement
+from django_random_id import RandomIDModel
 
 # Create your models here.
-class Booking(models.Model):
+class Booking(RandomIDModel):
   advertisement = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
   check_in_date = models.DateField()
   check_out_date = models.DateField()
