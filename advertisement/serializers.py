ac69434a74911c4505e4ffd34a61f0ffe9dd7532
kistgab <gabrielkist02@gmail.com>
2024-03-27 07:42:23 -0300

    feat: add property_info in advertisement

--- advertisement/serializers.py
+++ advertisement/serializers.py
@@ -4,6 +4,7 @@ from property.models import Property
 from property.serializers import PropertySerializer
 
 class AdvertisementSerializer(serializers.ModelSerializer):
+  property_info = PropertySerializer(source='property', read_only=True)
   class Meta:
     model = Advertisement
     fields = '__all__'
