bb56347c58a9e139bfbc68be53a498fa9eb6b44c
kistgab <gabrielkist02@gmail.com>
2024-03-27 08:04:56 -0300

    feat: hide foreign key but keep still editable

--- advertisement/serializers.py
+++ advertisement/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from advertisement.models import Advertisement
-from property.models import Property
 from property.serializers import PropertySerializer
 
 class AdvertisementSerializer(serializers.ModelSerializer):
   property_info = PropertySerializer(source='property', read_only=True)
   class Meta:
     model = Advertisement
+    extra_kwargs = {'property': {'write_only': True}}
     fields = '__all__'
--- booking/serializers.py
+++ booking/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
+from advertisement.serializers import AdvertisementSerializer
 from booking.models import Booking
 
 class BookingSerializer(serializers.ModelSerializer):
+  advertisement_info = AdvertisementSerializer(source='advertisement', read_only=True)
   class Meta:
     model = Booking
     fields = '__all__'
-    # depth = 1
+    extra_kwargs = {'advertisement': {'write_only': True}}
 
   def validate(self, data):
     if data["check_in_date"] > data["check_out_date"]:
